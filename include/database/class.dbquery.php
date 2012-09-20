3fb821830fb2fa1d33fe8834544d355e4341575e
Noé Martínez <noe.martinez@klanestudio.com>
2012-09-20 09:26:23 -0600

    Agregamos una consulta personalizada a la classdbquery

--- include/database/class.dbquery.php
+++ include/database/class.dbquery.php
@@ -1,5 +1,39 @@
 <?php
 
-class DBQuery {
-	
+
+class DBQuery extends DBManager{
+    
+      public function __construct()
+                        
+                {
+                    
+		$this->checkConnection();
+                                
+                }
+                              
+         /**
+	 * Ejecuta una sentencia SQL personalizada
+	 * @param string $table, $campo, $condition
+	 * @return boolean|mysqli_result
+	 * @throws QueryException
+	 */
+                
+     public function query_personalize($table,$campo,$condition) 
+                {
+         
+         $personalize= new DBManager();
+          
+         $query_person=$personalize->query("select $campo from $table where $condition");
+         
+         return $query_person;
+                
+         
+                }
+                
+                
+    
+  
+    
+    
 }
+//fin del archivo
