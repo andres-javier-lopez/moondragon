3f7792ac84e7400ff8c392657dedcceb4a0229c0
Andrés Javier López <ajavier.lopez@gmail.com>
2012-10-26 11:32:48 -0600

    fallback en caso de que gettext no este activado en el sistema

--- include/core/locale.php
+++ include/core/locale.php
@@ -5,8 +5,22 @@ $locale = isset($_GET['lang'])?$_GET['lang']:'es_SV';
 putenv("LANG=$locale.utf-8");
 setlocale(LC_ALL, "$locale.utf-8");
 
-$dom = bindtextdomain("messages", realpath(MOONDRAGON_PATH."/locale"));
-textdomain("messages");
+if(function_exists('bindtextdomain') && function_exists('textdomain')) {
+	$dom = bindtextdomain("messages", realpath(MOONDRAGON_PATH."/locale"));
+	textdomain("messages");
+	assert('textdomain(NULL) == "messages"');
+	assert('$dom == realpath(MOONDRAGON_PATH."/locale")');
+}
 
-assert('textdomain(NULL) == "messages"');
-assert('$dom == realpath(MOONDRAGON_PATH."/locale")');
+if(!function_exists('gettext')) {
+	trigger_error('No hay soporte para gettext', E_USER_WARNING);
+	function _($string) {
+		return $string;
+	}
+	
+	function gettext($string) {
+		return $string;
+	}
+}
+
+assert('function_exists("_")');
